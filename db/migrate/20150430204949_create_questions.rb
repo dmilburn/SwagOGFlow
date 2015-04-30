41f1ace1eb121b082d5ec24795429cfe0bb49139
grantziolkowski <grant.ziolkowski@gmail.com>
2015-04-30 17:11:46 -0400

    Questions Migration

--- db/migrate/20150430204949_create_questions.rb
+++ db/migrate/20150430204949_create_questions.rb
@@ -1,6 +1,12 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
+      t.string :title, null: false
+      t.text :body, null: false
+      t.boolean :answered, default: false
+      t.references :user
+      t.timestamps
+
     end
   end
 end
