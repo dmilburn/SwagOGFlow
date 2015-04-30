cbc554344f532df72404653d26f847df4022139a
grantziolkowski <grant.ziolkowski@gmail.com>
2015-04-30 18:34:21 -0400

    Add nested resources for questions and answers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :questions, :except => [:destroy] do
+    resources :answers, :except => [:destroy]
+  end
 
   resource :session, :only => [:add, :create, :destroy]
   resources :users, :except => [:destroy]
