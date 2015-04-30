0c77fdfd1f1c67a434fb94b8ef954e215c694fff
grantziolkowski <grant.ziolkowski@gmail.com>
2015-04-30 17:20:40 -0400

    Responses Migration

--- db/migrate/20150430205006_create_responses.rb
+++ db/migrate/20150430205006_create_responses.rb
@@ -1,6 +1,10 @@
 class CreateResponses < ActiveRecord::Migration
   def change
     create_table :responses do |t|
+      t.text :body, null: false
+      t.references :user
+      t.references :respondable, :polymorphic => true
+      t.timestamps
     end
   end
 end
