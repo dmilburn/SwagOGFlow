90f493b258c0b314eacb7b0de7e1b6f12ac7834a
grantziolkowski <grant.ziolkowski@gmail.com>
2015-04-30 17:02:15 -0400

    Question Model

---
+++ app/models/question.rb
@@ -0,0 +1,8 @@
+class Question < ActiveRecord::Base
+ has_many :answers
+ has_many :responses, as: :respondable
+ has_many :question_tags
+ has_many :tags, through: :question_tags
+ has_many :votes, as: :votable
+ belongs_to :user
+end
