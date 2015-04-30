c636e260b5f8074b4231ace519e4eafceaf0e716
grantziolkowski <grant.ziolkowski@gmail.com>
2015-04-30 19:02:09 -0400

    Add user create method users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,17 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
   def create
-    @user = User.build(user_params)
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_url # :notice => "Signed up!"
+    else
+      render "new"
+    end
   end
+
   def index
   end
   def show
