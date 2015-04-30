cab16eff0b8a5ec833ae46b51619b33dba3383c0
grantziolkowski <grant.ziolkowski@gmail.com>
2015-04-30 17:07:49 -0400

    Answers Migration

--- db/migrate/20150430204727_create_answers.rb
+++ db/migrate/20150430204727_create_answers.rb
@@ -1,6 +1,11 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
+      t.text :body, null: false
+      t.references :question
+      t.references :user
+      t.timestamps
     end
   end
 end
+
