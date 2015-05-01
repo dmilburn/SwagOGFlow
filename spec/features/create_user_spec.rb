cbaea5b5cee58cf122b2ba09d1088e992f01ccfa
Carl Swanson <cetswanson@gmail.com>
2015-05-01 14:26:29 -0400

    Add successful signup test

--- spec/features/create_user_spec.rb
+++ spec/features/create_user_spec.rb
@@ -7,4 +7,14 @@ RSpec.feature "User Sign Up", :type => :feature do
     expect(page).to have_content("Password")
     expect(page).to have_content("Password confirmation")
   end
+
+  scenario "A successful signup redirects to the root page" do
+    visit signup_path
+    fill_in "user_name", :with => "test_user"
+    fill_in "user_password", :with => "password"
+    fill_in "user_password_confirmation", :with => "password"
+    click_on "Create User"
+    expect(page).to have_content("Sign Out")
+  end
+
 end
