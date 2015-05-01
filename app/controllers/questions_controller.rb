ef5019e1bd1d82b239d14e060330e6ff1d2aad80
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-01 19:10:30 -0400

    Add logic to edit and update routes in questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -32,9 +32,16 @@ class QuestionsController < ApplicationController
   end
 
   def edit
+    @question = Question.find(params[:id])
   end
 
   def update
+    question = Question.find(params[:id])
+    if question.update_attributes(question_params)
+      redirect_to question_path(question)
+    else
+      redirect_to edit_question_path(question)
+    end
   end
 
   private
