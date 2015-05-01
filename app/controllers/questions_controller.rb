1a0ded66a1874740ba23870ef419820b8d2edf00
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-01 11:54:00 -0400

    Add logic to questions create method

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,12 +9,17 @@ class QuestionsController < ApplicationController
 	end
 
 	def create
+		question = current_user.questions.build(question_params)
+    if question.save
+      flash[:notice] = "Your question has been posted!"
+      redirect_to question_path(question)
+    else
+      flash[:notice] = "Sorry, your question didn't save. Please try again."
+      redirect_to new_question_path
+    end
 	end
 
 	private
 
 	def question_params
-    params.require(:question).permit(:title, :body, :user_id)
-  end
-
 end
