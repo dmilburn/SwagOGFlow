6f0dcc462da6ad45a3d4d47c14c72fc608c14c6f
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-01 18:12:50 -0400

    Change get votable path to get path

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,7 +4,7 @@ class VotesController < ApplicationController
     type_instance = votable_type.find(params[:vote][:votable_id])
     vote = type_instance.votes.create(vote_params)
     vote.voter_id = current_user.id
-    redirect_to type_instance.get_votable_path
+    redirect_to type_instance.get_path
   end
 
   def vote_params
