f58cb33a51f6607f3ce35345d1a056e9989489e7
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-01 16:51:10 -0400

    Add vote params to votes controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,12 +1,13 @@
 class VotesController < ApplicationController
   def create
-    question = Question.find(params[:question_id])
-    vote = question.votes.build(params[:vote])
     vote.voter_id = current_user.id
     if vote.save
-      redirect_to question_path(question)
     else
       redirect_to question_path(question)
     end
+  end
+
+  def vote_params
+    params.require(:vote).permit(:voter_id, :votable_type, :votable_id, :value)
   end
 end
