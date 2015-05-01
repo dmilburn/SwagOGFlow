d3ea09c8a0b798daf6c959a8972eca2ba1e8dd70
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-01 18:09:10 -0400

    Refactor create votes method to not descriminate questions, answers

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,9 +1,10 @@
 class VotesController < ApplicationController
   def create
-    answer = Answer.find(params[:vote][:votable_id])
-    vote = answer.votes.create(vote_params)
+    votable_type = (params[:vote][:votable_type]).constantize
+    type_instance = votable_type.find(params[:vote][:votable_id])
+    vote = type_instance.votes.create(vote_params)
     vote.voter_id = current_user.id
-    redirect_to question_path(answer.question)
+    redirect_to type_instance.get_votable_path
   end
 
   def vote_params
