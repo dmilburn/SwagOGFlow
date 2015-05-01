5c01cdf974c2784e7404ae18517d2f4d25fb5d17
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-01 16:30:32 -0400

    Add create votes logic to votes controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,5 +1,14 @@
 class VotesController < ApplicationController
   def create
-
+   def create
+    question = Question.find(params[:question_id])
+    vote = question.votes.build(params[:vote])
+    vote.voter_id = current_user.id
+    if vote.save
+      redirect_to question_path(question)
+    else
+      redirect_to question_path(question)
+    end
+  end
   end
 end
