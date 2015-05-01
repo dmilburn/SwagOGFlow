54f99a7ab704f96d51883648a6252c8b0b72bc7f
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-01 11:41:28 -0400

    Add controller test for questions controller index route

---
+++ spec/controllers/questions_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec-helper'
+
+describe QuestionsController do
+  context "#index" do
+    it "renders successfully to the index view" do
+      get :index
+      expect(response).to be_success
+    end
+  end
+end
