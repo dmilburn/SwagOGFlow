a8cb28da87830e1950811a441d17c2fdc2c941cd
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-01 17:07:41 -0400

    Sort the questions index from newest to oldest

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,7 @@ class QuestionsController < ApplicationController
   before_action :gate_keeper, only: [:new]
 
   def index
-    @questions = Question.all
+    @questions = Question.all.order(created_at: :desc)
   end
 
   def new
