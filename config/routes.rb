e17ab56577b5ee2411478cc70386b33d8cc9133a
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-01 11:05:20 -0400

    Add user auth route aliases

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   resource :session, :only => [:new, :create, :destroy]
   resources :users, :except => [:destroy]
 
+  get 'signin' => 'sessions#new', as: 'signin'
+  get 'signout' => 'sessions#destroy', as: 'signout'
+  get 'signup' => 'users#new', as: 'signup'
+
   root 'questions#index'
 end
