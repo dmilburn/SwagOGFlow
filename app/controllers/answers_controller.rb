0ebb6322c3a81a5b35eecedd3a544961694d340e
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-01 14:50:22 -0400

    Add complete create answers method to answers controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,13 @@
 class AnswersController < ApplicationController
   def create
+    question = Question.find(params[:question_id])
+    answer = question.answers.build(answer_params)
+    answer.user_id = current_user.id
+    if answer.save
+      redirect_to question_path(question)
+    else
+      redirect_to new_question_answer_path
+    end
   end
 
   private
