916af42612bb6029c686e3497d4277960af75c3d
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-01 15:51:15 -0400

    Add count_votes method logic to answer method

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,10 +1,10 @@
 class Answer < ActiveRecord::Base
- has_many :responses, as: :respondable
- has_many :votes, as: :votable
- belongs_to :question
- belongs_to :user
+  has_many :responses, as: :respondable
+  has_many :votes, as: :votable
+  belongs_to :question
+  belongs_to :user
 
   def count_votes
-
+    !self.votes.first  ? 0 : self.votes.first.value
   end
 end
