c669ad4b72a3826a14be6c394c4939a1f576c5ea
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-01 12:50:43 -0400

    Debug signup by adding flash notices and a password_confirmation field to user_params method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_url # :notice => "Signed up!"
+      redirect_to root_url flash[:notice] = "You're signed up!"
     else
+      flash[:notice] = "Unable to create user, please try again."
       render "new"
     end
   end
@@ -28,9 +29,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :password)
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
-
-
 
 end
