2cb6fcda1b7e6dcef55fdf7f70cee77545a366d9
Carl Swanson <cetswanson@gmail.com>
2015-05-01 15:38:56 -0400

    Add unsuccesseful sign in feature test

--- spec/features/user_signin_spec.rb
+++ spec/features/user_signin_spec.rb
@@ -6,4 +6,15 @@ RSpec.feature "User Sign Ip", :type => :feature do
     expect(page).to have_content("Name")
     expect(page).to have_content("Password")
   end
+
+  scenario "A successful sign in redirects to the root" do
+    User.create(name: "test_user",
+                password: "password",
+                password_confirmation: "password")
+    visit signin_path
+    fill_in "session_name", :with => "test_user"
+    fill_in "session_password", :with => "password"
+    click_on "Save Session"
+    expect(page).to have_content("Logged in!")
+  end
 end
