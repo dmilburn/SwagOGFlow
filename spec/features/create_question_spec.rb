1e67d33432a45ab8668bda766267181eb5c150ff
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-02 13:04:58 -0400

    Change it message in first adding tags test in new question features test

--- spec/features/create_question_spec.rb
+++ spec/features/create_question_spec.rb
@@ -3,11 +3,12 @@ require 'spec_helper'
 feature 'New Question' do
   let(:user) { User.create(name: "username", password: '123', password_confirmation: '123')}
   context "Adding tags" do
-    it "should allow you to create tags" do
+    it "should display the tags field to add tags" do
       stub_current_user(user)
       stub_authorize_user!
       visit new_question_path
       expect(page).to have_content "Tags"
     end
+
   end
 end
