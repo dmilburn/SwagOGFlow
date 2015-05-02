7e89bdf7e0a36444ce2157719e071a2cc244595c
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-02 12:58:47 -0400

    Add create user to New question feature test

--- spec/features/create_question_spec.rb
+++ spec/features/create_question_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 feature 'New Question' do
+  let(:user) { User.create(name: "username", password: '123', password_confirmation: '123')}
   context "A user can create a new question" do
     it "should allow you to create tags" do
       stub_authorize_user!
