44a861fd553b7698403792ff1a205ec912373e63
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-02 12:56:22 -0400

    Add include all support files into spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,7 @@ Capybara.javascript_driver = :chrome
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
 
