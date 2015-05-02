5a58515f10bcc3c0694e7901459df56086d7ee43
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-02 12:02:52 -0400

    Add question double logic to tag model spec to test for association

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,7 +1,13 @@
 require 'spec_helper'
 describe Tag do
+  let!(:question) {double("Question", title: "Stuck", body: "Need Help")}
   let(:tag) { FactoryGirl.build :tag }
   it "should have a name" do
     expect(tag.name).to eq "test_tag"
+  end
+  it "can be assigned to a question through belongs to association" do
+    expect {
+      question.tags << tag
+      }.to change {question.tags.count}.from(0).to(1)
   end
 end
