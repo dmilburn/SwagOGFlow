f227a9d2239823caddef6b5a935dc47581d7b64c
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-02 12:59:51 -0400

    Add stub_current_user helper to authentication helpers in spec support

--- spec/support/authentication_helpers.rb
+++ spec/support/authentication_helpers.rb
@@ -3,3 +3,7 @@ module AuthenticationHelpers
     ApplicationController.any_instance.stub(:authorize_user!) { true }
   end
 end
+
+RSpec.configure do |config|
+  config.include AuthenticationHelpers, :type => :feature
+end
