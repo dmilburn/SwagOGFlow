ab13f2445639c9cb216d99c67c221004ac61aa30
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-02 11:36:03 -0400

    Change render to redirect in questions controller spec

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 describe QuestionsController do
   context "#index" do
-    it "renders successfully to the index view" do
+    it "redirects successfully to the index view" do
       get :index
       expect(response).to be_success
     end
