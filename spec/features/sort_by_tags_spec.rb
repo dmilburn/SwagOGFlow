79274801a9eb8881fbc051b837749ab90c9660f5
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-03 14:27:34 -0400

    Write and pass sort_by_tags test for List of tags link content on index page

--- spec/features/sort_by_tags_spec.rb
+++ spec/features/sort_by_tags_spec.rb
@@ -1,1 +1,9 @@
-sort_by_tags_spec.rb
+feature 'Sort by Tags' do
+  context "Navigate from index page to tags page" do
+    it "should display the list of tags link" do
+      visit root_path
+      expect(page).to have_content("List of tags")
+    end
+
+  end
+end
