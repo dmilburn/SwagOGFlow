6deab20966e9c1765423de696a1be9a9d06ca7b9
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-02 20:25:16 -0400

    Add created ago helper method in helpers folder for timestamp

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def created_ago(creation)
+    seconds_ago = Time.now - creation.created_at
+    if seconds_ago > 86400
+      (seconds_ago / 86400).to_i.to_s + " days ago"
+    else
+      (seconds_ago / 3600).to_i.to_s + " hours ago"
+    end
+  end
 end
