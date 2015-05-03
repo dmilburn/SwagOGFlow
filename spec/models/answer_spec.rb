3d5ada9385fee1f3ae80f9b3b5b5585b2a5e9d06
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-03 13:32:50 -0400

    Add logic to answer model test to create answer and assign it to question

---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+describe Answer do
+  let!(:question) {Question.create(title: "Stuck", body: "Need Help")}
+  let(:answer) { FactoryGirl.build :answer }
+  it "should have a name" do
+    expect(answer.name).to eq "test_answer"
+  end
+  it "can be assigned to a question through belongs to association" do
+    expect {
+      question.answers << answer
+      }.to change {question.answers.count}.from(0).to(1)
+  end
+end
