8feee6db72fbcd39a8c58060c1a0ea792c715fe1
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-03 15:20:01 -0400

    Eliminate before save helper in vote model since current user helper was not accessible

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,9 +1,4 @@
 class Vote < ActiveRecord::Base
-  before_save :set_user
   belongs_to :voter, class_name: "User"
   belongs_to :votable, polymorphic: true
-
-  def set_user
-    self.voter_id = current_user.id
-  end
 end
