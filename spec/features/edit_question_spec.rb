362fd0f91bac8741bbb91a708082ad7be391beba
Carl Swanson <cetswanson@gmail.com>
2015-05-03 11:35:49 -0400

    Add question owner editing form view test

--- spec/features/edit_question_spec.rb
+++ spec/features/edit_question_spec.rb
@@ -12,13 +12,14 @@ RSpec.feature "Question Editing", :type => :feature do
     expect(page).not_to have_content "Edit Question"
   end
 
-    user = User.create(name: "test_user",
-                  password: "password",
-                  password_confirmation: "password")
+  scenario "A user can view the edit form on questions they have created" do
     user.questions.create(title: "Testing Questions",
                          body: "SwagOGFlow Question")
+    stub_current_user(user)
     visit root_path
     click_on "Testing Questions"
-    expect(page).not_to have_content "Edit Question"
+    click_on "Edit Question"
+    expect(page).to have_content "Edit Question"
+    expect(page).to have_content "Title"
   end
 end
