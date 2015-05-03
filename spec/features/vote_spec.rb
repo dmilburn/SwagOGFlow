a6e7ae08930ffcdd3019142d99f07c3bc1efed24
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-03 17:12:48 -0400

    Add test for upvote to vote spec features test

--- spec/features/vote_spec.rb
+++ spec/features/vote_spec.rb
@@ -1,1 +1,14 @@
-vote_spec.rb
+require 'spec_helper'
+
+feature 'Voting' do
+  let(:user) { User.create(name: "username", password: '123', password_confirmation: '123')}
+  context "Signed in user can vote" do
+    it "should allow a user to upvote a question" do
+      user.questions.create(title: "question_title", body: "quesiton_body")
+      stub_current_user(user)
+      visit root_path
+      click_on("Upvote")
+      expect(page).to have_content "1"
+    end
+  end
+end
