20ed542410e8af3700c6431a7e4c39fbf2694060
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-03 13:49:30 -0400

    Pass test for search bar on root path with have_button capybara selector

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -2,7 +2,7 @@ feature 'Search' do
   context "Searching from root page" do
     it "should display search bar" do
       visit root_path
-      expect(page).to have_content "Search"
+      expect(page).to have_button("Search")
     end
 
 
