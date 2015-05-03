28036572e81835287dc401515bfb0fb3fe9d5f1e
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-03 17:20:11 -0400

    Remove only one vote to second test in vote features spec

--- spec/features/vote_spec.rb
+++ spec/features/vote_spec.rb
@@ -4,7 +4,7 @@ feature 'Voting' do
   let(:user) { User.create(name: "username", password: '123', password_confirmation: '123')}
   let!(:question) { Question.create(title: "question_title", body: "quesiton_body", user_id: user.id)}
   context "Signed in user can vote" do
-    it "should allow a user to upvote a question only once" do
+    it "should allow a user to upvote a question" do
       stub_current_user(user)
       visit root_path
       click_on("Upvote")
