fa9eb0c4a3b4d5930a310e847fdb7a69d7d27983
Carl Swanson <cetswanson@gmail.com>
2015-05-03 11:31:53 -0400

    Refactor user question edit tests with let statement

--- spec/features/edit_question_spec.rb
+++ spec/features/edit_question_spec.rb
@@ -1,7 +1,17 @@
 require 'spec_helper'
 
 RSpec.feature "Question Editing", :type => :feature do
+
+  let(:user) { User.create(name: "username", password: '123', password_confirmation: '123')}
+
   scenario "A user cannot view the edit form on questions they have not created" do
+    user.questions.create(title: "Testing Questions",
+                         body: "SwagOGFlow Question")
+    visit root_path
+    click_on "Testing Questions"
+    expect(page).not_to have_content "Edit Question"
+  end
+
     user = User.create(name: "test_user",
                   password: "password",
                   password_confirmation: "password")
