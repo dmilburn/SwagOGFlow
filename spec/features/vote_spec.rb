d1aefb7de6a7f3cbc335bf6702ea428f0f3a2709
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-03 17:17:43 -0400

    Move question creation in vote feature spec from inside it block to top level

--- spec/features/vote_spec.rb
+++ spec/features/vote_spec.rb
@@ -3,8 +3,13 @@ require 'spec_helper'
 feature 'Voting' do
   let(:user) { User.create(name: "username", password: '123', password_confirmation: '123')}
   context "Signed in user can vote" do
-    it "should allow a user to upvote a question" do
-      user.questions.create(title: "question_title", body: "quesiton_body")
+    it "should allow a user to upvote a question only once" do
+      stub_current_user(user)
+      visit root_path
+      click_on("Upvote")
+      expect(page).to have_content "1"
+    end
+    it "should not allow the same user to vote on the same question again" do
       stub_current_user(user)
       visit root_path
       click_on("Upvote")
