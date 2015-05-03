0e4ae62d848750367fab0087d69dcb5be7dae90c
Carl Swanson <cetswanson@gmail.com>
2015-05-03 11:15:13 -0400

    Correct indenting on question show test

--- spec/features/show_question_spec.rb
+++ spec/features/show_question_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 RSpec.feature "Question Viewing", :type => :feature do
   scenario "A user can see questions on the root path" do
-  user = User.create(name: "test_user",
-                password: "password",
-                password_confirmation: "password")
-  user.questions.create(title: "Testing Questions",
-                       body: "SwagOGFlow Question")
-  visit root_path
-  expect(page).to have_content "Testing Questions"
+    user = User.create(name: "test_user",
+                  password: "password",
+                  password_confirmation: "password")
+    user.questions.create(title: "Testing Questions",
+                         body: "SwagOGFlow Question")
+    visit root_path
+    expect(page).to have_content "Testing Questions"
   end
 end
