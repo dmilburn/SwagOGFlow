63077098358fdfc9d14401c83379f9f7fdedcbf7
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-03 13:45:26 -0400

    Add feature test for search bar visible on root page

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -1,1 +1,10 @@
-search_spec.rb
+feature 'Search' do
+  context "Searching from root page" do
+    it "should display search bar" do
+      visit root_path
+      expect(page).to have_content "Search"
+    end
+
+
+  end
+end
