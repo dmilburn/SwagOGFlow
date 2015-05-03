626333103035dbc4fc2dd79dcf8b52459a26a2c2
Carl Swanson <cetswanson@gmail.com>
2015-05-03 11:17:26 -0400

    Add individual question view feature test

--- spec/features/show_question_spec.rb
+++ spec/features/show_question_spec.rb
@@ -10,4 +10,15 @@ RSpec.feature "Question Viewing", :type => :feature do
     visit root_path
     expect(page).to have_content "Testing Questions"
   end
+
+  scenario "A user can click on a question and view its body" do
+    user = User.create(name: "test_user",
+                  password: "password",
+                  password_confirmation: "password")
+    user.questions.create(title: "Testing Questions",
+                          body: "SwagOGFlow Question Body")
+    visit root_path
+    click_on "Testing Questions"
+    expect(page).to have_content "SwagOGFlow Question Body"
+  end
 end
